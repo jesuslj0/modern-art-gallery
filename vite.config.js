8deb13e51a1c7950d5657673899ddbfce0daf2cf
Jesús <jesuslopj0@gmail.com>
2025-06-25 19:55:59 +0200

    Configurando múltiples páginas para Vite en build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   base: '/projects/css-sass/modern-art-gallery/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        location: path.resolve(__dirname, 'location.html'),
+      }
+    }
+  }
 })
